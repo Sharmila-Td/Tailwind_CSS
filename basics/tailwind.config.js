8a92a01cdc7d92ef592fa102e05f5a0258752927
sharmila-developer <sharmi.webdev@gmail.com>
2024-08-27 21:31:10 -0700

    first commit

---
+++ basics/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'pure-pink':'#ff5689'
+      },
+    },
+    fontFamily:{
+      Mate:["Matemasie", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
+
