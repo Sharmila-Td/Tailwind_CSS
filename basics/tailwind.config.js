89d8d2464e2dc3d878d8c49ee53767bebad09c34
sharmila-developer <sharmi.webdev@gmail.com>
2024-08-28 10:59:04 -0700

    basics finished

--- basics/tailwind.config.js
+++ basics/tailwind.config.js
@@ -4,13 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'pure-pink':'#ff5689'
+        'pure-pink':'#ff5689',
+        'dark-blue':'#0206A5'
       },
+      fontWeight:{
+        'bold500' : 500
+      }
     },
     fontFamily:{
       Mate:["Matemasie", "sans-serif"]
+    },
+    screens:{
+      'tablet':'640px', //@media (min-width: 640px)
+      'laptop': '1024px', //@media (min-width: 1024px)
+      'desktop': '1280px'
     }
   },
   plugins: [],
+  darkMode:"selector"
 }
 
